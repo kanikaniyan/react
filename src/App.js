5c8eb181f41ae564f298b1be235ed9ae0b838802
kkaniyan <kani@xejic.com>
2023-08-14 11:17:41 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Configure,
+  Hits,
+  SearchBox,
+  Panel,
+  RefinementList,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="header">
+        <h1 className="header-title">
+          <a href="/">Getting started</a>
+        </h1>
+        <p className="header-subtitle">
+          using{' '}
+          <a href="https://github.com/algolia/instantsearch/tree/master/packages/react-instantsearch-dom">
+            React InstantSearch
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="container">
+        <InstantSearch searchClient={searchClient} indexName="instant_search">
+          <Configure hitsPerPage={8} />
+          <div className="search-panel">
+            <div className="search-panel__filters">
+              <Panel header="brand">
+                <RefinementList attribute="brand" />
+              </Panel>
+            </div>
+
+            <div className="search-panel__results">
+              <SearchBox
+                className="searchbox"
+                translations={{
+                  placeholder: '',
+                }}
+              />
+              <Hits hitComponent={Hit} />
+
+              <div className="pagination">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
     </div>
+  );
+}
+
+function Hit(props) {
+  return (
+    <article>
+      <h1>
+        <Highlight attribute="name" hit={props.hit} />
+      </h1>
+      <p>
+        <Highlight attribute="description" hit={props.hit} />
+      </p>
+    </article>
   );
 }
 
